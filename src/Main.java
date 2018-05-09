1e149be31294f8b1a2fea59b9a0bcd1fa076272a
Baumgartner_Lukas <baumgartner.webtree@gmail.com>
2018-05-09 09:43:18 +0200

    Implemented d ToDo: - avgWordCount

--- src/Main.java
+++ src/Main.java
@@ -3,48 +3,73 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.counting;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         final String filePath = "sampletext.txt";
+        final String faustPath = "faust_1.txt";
         final String outPath = "outputtext.txt";
+        final String faustOutPath = "outputfaus.txt";
 
 
-        Optional<String> line = findLine(filePath, "five");
-        Optional<String> line2 = findLine(filePath, "not in text");
+
         System.out.println("Find first line with: five");
-        System.out.println(line);
+        System.out.println(findLine(filePath, "five"));
         System.out.println("Find first line with: not in text");
-        System.out.println(line2);
+        System.out.println(findLine(filePath, "not in text"));
 
-        List<String> lines = findLines(filePath, "is");
-        List<String> lines2 = findLines(filePath, "isn't");
+        System.out.println("Find first line with: Vorspiel");
+        System.out.println(findLine(faustPath, "Vorspiel"));
+        System.out.println("Find first line with: Spaß");
+        System.out.println(findLine(faustPath, "Spaß"));
+
         System.out.println("Find lines with: is");
-        System.out.println(lines);
+        findLines(filePath, "is").forEach(System.out::println);
+        System.out.println("Find lines with: Herz");
+        findLines(faustPath, "Herz").forEach(System.out::println);
         System.out.println("Find lines with: isn't");
-        System.out.println(lines2);
+        findLines(filePath, "isn't").forEach(System.out::println);
 
         writeNoEmptyLines(filePath, outPath);
-        System.out.println(words(filePath));
+        writeNoEmptyLines(faustPath, faustOutPath);
 
+        System.out.println("Get all words used in the text");
+        words(filePath).forEach(System.out::println);
+        words(faustPath).forEach(System.out::println);
+
+        System.out.println("Get avg line length");
         System.out.println(averageLineLength(filePath));
+        System.out.println(averageLineLength(faustPath));
+
+//        System.out.println("Get average nr of words in line");
+//        System.out.println(averageWordsInLine(filePath));
+        System.out.println("group words by their first character");
+        System.out.println(alphaGrouping(filePath));
+        System.out.println(alphaGrouping(faustPath));
 
     }
     //-------------------------------- a --------------------------------
     static Optional<String> findLine(String inFile, String text) throws IOException {
         return Files.lines(Paths.get(inFile))
-                //regex to find the exact word not a substring as with contains
+                //the regex beneath is used to find the exact word in the text
+                //and not a substring as with contains
+                //source: http://www.vogella.com/tutorials/JavaRegularExpressions/article.html
                 .filter(w -> w.matches("(?s).*\\b" + text + "\\b.*"))
                 .findFirst();
     }
 
     static List<String> findLines(String inFile, String text) throws IOException {
         return Files.lines(Paths.get(inFile))
-                //regex to find the exact word not a substring as with contains
+                //the regex beneath is used to find the exact word in the text
+                //and not a substring as with contains
                 //source: http://www.vogella.com/tutorials/JavaRegularExpressions/article.html
                 .filter(w -> w.matches("(?s).*\\b" + text + "\\b.*"))
                 .collect(Collectors.toList());
@@ -62,12 +87,10 @@ public class Main {
     //-------------------------------- c --------------------------------
     static Stream<String> wordStream(String inFile) throws IOException{
         return Files.lines(Paths.get(inFile))
-                .map(String::trim)
-                .filter(line -> !line.isEmpty())
-                .filter(word -> !word.matches("[^A-Za-z]"))
-                //.map(String::toLowerCase)
-                .map(line -> line.split("[ .,;?!:()-]"))
-                .flatMap(Arrays::stream)
+                .flatMap(line -> Stream.of(line.split("[ .,;?!:()-]")))
+                .filter(word -> word.length() > 0)
+                .map(String::toLowerCase)
+                .sorted(String::compareTo)
                 .distinct();
     }
 
@@ -78,7 +101,28 @@ public class Main {
 
     //-------------------------------- d --------------------------------
     static double averageLineLength(String inFile) throws IOException{
+        return Files.lines(Paths.get(inFile))
+                //disclaimer: does not count empty lines as a line
+                //i.e. sampletext.txt has 7 lines (2 empty) => 5 lines with avg. 29.4 chars
+                .filter(line -> line.trim().length() > 0)
+                .collect(Collectors.averagingDouble(String::length));
+    }
+
+    static double averageWordsInLine (String inFile) throws IOException{
         return 0;
+        //ToDo: implement
+//        return Files.lines(Paths.get(inFile))
+//                .flatMap(line -> Stream.of(line.split("[ .,;?!:()-]")))
+//                .filter(word -> word.length() > 0)
+//                .count();
+    }
+
+    //-------------------------------- e --------------------------------
+    static Map<Character, List<String>> alphaGrouping(String inFile) throws IOException{
+        return wordStream(inFile)
+                .filter(w -> w.length() > 0)
+                .map(String::toLowerCase)
+                .collect(Collectors.groupingBy(s -> s.charAt(0)));
     }
 
 }
